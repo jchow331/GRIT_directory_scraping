f6c303d6f874cd6cbcd6528d16de61793ebe6bdc
[jchow331] <[jordan.m.chow@gmail.com]>
2024-12-20 15:08:07 -0500

    updated files

--- canada_yp_cleaning.py
+++ canada_yp_cleaning.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('canadayp_output_1.csv')
+df = pd.read_csv(r'C:\Users\kumar\OneDrive\Desktop\Jordan\GRIT_directory_scraping\canadayp_output_clean.csv')
 
 df_dict = df.groupby(['name'])['category'].apply(', '.join).to_dict()#.reset_index()
 #df_dict = df_dict.set_index('name').to_dict()
@@ -13,8 +13,4 @@ for i in range(0,len(df)):
         pass
 
 df.drop_duplicates(inplace=True)
-
-df.to_csv('canadayp_output_clean_1.csv',index=False, encoding='utf_8_sig')
-
-
-#df.loc[i,'category'] = [x.strip() for x in df.loc[i,'category'].split(',')]
+df.to_csv('canadayp_output_clean.csv',index=False, encoding='utf_8_sig')
