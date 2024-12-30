b0665e3740fbc59cd03b2129da15d9c65c44bc84
[jchow331] <jordan.m.chow@gmail.com>
2024-12-30 15:25:06 -0500

    finished scraper

--- coo_optometrists/coo_optometrists.py
+++ coo_optometrists/coo_optometrists.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 import pandas as pd
+import time
 import re
 
 def myzip(*args):
@@ -42,15 +44,29 @@ def scrape_page(soup):
     buffer = [element for innerList in buffer for element in innerList]
     buffer = [[next(iter(y), None) for y in x] for x in buffer]
 
-
+    return buffer
     
 
-
+#Initiate webdriver
 driver = webdriver.Chrome()
 driver.get('https://members.collegeoptom.on.ca/COO/PublicDirectory/Public_Directory_Member_Process/Public_Register/PublicRegisterMemberProcess.aspx?hkey=349c676e-cbe0-4052-927d-08cc49d4f537')
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-all_info = []
-all_info.append(scrape_page(soup))
+#Get number of pages
+#soup.find('select',{'name':'ctl01$TemplateBody$WebPartManager1$gwpste_container_MainControl$ciMainControl$ddlSearchResultsPage'})
+pages = [x['value'] for x in soup.find_all('option')]
 
+all_info = []
+for page in pages:
+
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    if soup.find('option',{'selected':'selected'})['value'] != page:
+        time.sleep(10)
+
+    all_info.extend(scrape_page(soup))
+    next_button = driver.find_element(By.ID, 'ctl01_TemplateBody_WebPartManager1_gwpste_container_MainControl_ciMainControl_lbtnNext').click()
+
+df = pd.DataFrame(columns=['name','practice_location','phone_number'], data=all_info)   
+df.to_csv(r'C:\Users\kumar\OneDrive\Desktop\Jordan\GRIT_directory_scraping\coo_optometrists\coo_optometrists_output_raw.csv', index=False)
 
