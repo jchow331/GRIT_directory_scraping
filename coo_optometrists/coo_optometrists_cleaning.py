9ff02934670694c957d2f67792f00bc58a47474b
[jchow331] <jordan.m.chow@gmail.com>
2024-12-30 16:03:35 -0500

    updated cleaning script

--- coo_optometrists/coo_optometrists_cleaning.py
+++ coo_optometrists/coo_optometrists_cleaning.py
@@ -7,5 +7,17 @@ df = df.reset_index()
 
 df['phone_number'] = df['phone_number'].str.strip()
 df['phone_number'] = df['phone_number'].replace(r"\D+", "", regex=True).str.strip()
+df.loc[df['phone_number'].str.len() > 10, 'phone_number'] = df['phone_number'].str[1:]
 
-df = df[df['phone_number'] != '(000) 000-0000']
+df = df[df['phone_number'] != '0000000000']
+
+df = df.drop_duplicates(subset=['phone_number'], keep='first')
+
+df_905 = df[df['phone_number'].str[0:3] == '905']
+df_647 = df[df['phone_number'].str[0:3] == '647']
+df_416 = df[df['phone_number'].str[0:3] == '416']
+
+df_sorted = pd.concat([df_905, df_647, df_416, df])
+df_sorted = df_sorted.drop_duplicates(subset=['phone_number'], keep='first')
+
+df_sorted.to_csv(r'C:\Users\kumar\OneDrive\Desktop\Jordan\GRIT_directory_scraping\coo_optometrists\coo_optometrists_output_sorted_filtered.csv', index=False)
