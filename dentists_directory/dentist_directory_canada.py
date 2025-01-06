de1dd538f2cd2dd5b16407e25aad1cc49ea0ef17
[jchow331] <jordan.m.chow@gmail.com>
2025-01-06 12:28:05 -0500

    wrote dentist_directory_canada website scraper

--- dentists_directory/dentist_directory_canada.py
+++ dentists_directory/dentist_directory_canada.py
@@ -2,16 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 
 import pandas as pd
+import re
 
 
 def scrape_location(location):
-    pass
+
+    page = requests.get(url.format(location))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    listings = soup.find_all('div', {'class':'one_third'})[1:-2]
+    listings = [[next(iter(re.findall('<strong>(.+?)</strong>', str(x))), None),
+                location.title(),
+                next(iter(re.findall('Phone(.+?)\n', x.text)), None),
+                x.find('a')] for x in listings]
+    listings = [[x[0], x[1], x[2], x[3]['href']] if x[3] else x for x in listings]
+
+    return listings
 
 
 locations = ['markham', 'richmond-hill', 'scarborough', 'toronto']
-
 url = 'https://www.dentistdirectorycanada.ca/ontario-dentists-directory/{}-dentists-directory/'
 
-#for location in locations:
-    
-page = requests.get(url.format(location))
+all_data = []
+for location in locations:
+    all_data.extend(scrape_location(location))
+
+df = pd.DataFrame(columns=['name','location','phone_number','website'], data=all_data)
+
+df['phone_number'] = df['phone_number'].str.strip()
+df['phone_number'] = df['phone_number'].replace(r"\D+", "", regex=True).str.strip()
+
+df.to_csv(r'C:\Users\kumar\OneDrive\Desktop\Jordan\GRIT_directory_scraping\dentists_directory\dentists_directory_canada_output.csv', index=False)
